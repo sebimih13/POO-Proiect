99bacdf28fe1bda12634d0aeac8c9b4ac4dbf675
Mihalache Sebastian <51950623+sebimih13@users.noreply.github.com>
2023-03-23 18:25:43 +0200

    Initialize sfml

--- main.cpp
+++ main.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <cstdio>
 
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
+
 #include "src/Character.h"
 #include "src/Card.h"
 #include "src/Item.h"
 
-int main() 
-{
+#ifdef __linux__
+#include <X11/Xlib.h>
+#endif
+
+class SomeClass {
+public:
+    explicit SomeClass(int) {}
+};
+
+SomeClass *getC() {
+    return new SomeClass{2};
+}
+
+int main() {
+    #ifdef __linux__
+    XInitThreads();
+    #endif
+
+    SomeClass *c = getC();
+    std::cout << c << "\n";
+    delete c;
+
+    sf::RenderWindow window;
+    // NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:30
+    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
+    window.setVerticalSyncEnabled(true);
+    //window.setFramerateLimit(60);
+
+
     std::cout << "Hello, world!\n\n";
 
     // Player
@@ -47,5 +79,33 @@ int main()
     std::getchar();
 
     return 0;
-}
 
+
+
+    while(window.isOpen()) {
+        sf::Event e;
+        while(window.pollEvent(e)) {
+            switch(e.type) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::Resized:
+                std::cout << "New width: " << window.getSize().x << '\n'
+                          << "New height: " << window.getSize().y << '\n';
+                break;
+            case sf::Event::KeyPressed:
+                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
+                break;
+            default:
+                break;
+            }
+        }
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(300ms);
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
