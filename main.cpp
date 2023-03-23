867e81d86aea11d8f7aebe54b92c88c1498c7297
sebi <sebimih@gmail.com>
2023-03-23 21:40:51 +0200

    test 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <conio.h>
 
-int main() {
+int main() 
+{
     std::cout << "Hello, world!\n";
     std::cout << "test\n";
+
+    printf("Press any key...\n");
+    _getch();
+
     return 0;
 }
+
