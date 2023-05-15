195455ef8061777ddbbe3567181014f795e29961
Mihalache Sebastian <51950623+sebimih13@users.noreply.github.com>
2023-05-15 13:22:12 +0300

    fix

--- src/Singleton.cpp
+++ src/Singleton.cpp
@@ -1,24 +1,2 @@
 #include "Singleton.h"
 
-// Initialization of static const
-//template<typename T>
-//T Singleton<T>::Instance;
-
-//
-//#include "Game.h"
-//
-//template
-//class Singleton<Game>;
-//
-//template
-//Game Singleton<Game>::Instance;
-//
-//
-//#include "ResourceManager.h"
-//
-//template
-//class Singleton<ResourceManager>;
-//
-//template
-//ResourceManager Singleton<ResourceManager>::Instance;
-//
